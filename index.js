1e6c5e1f4fc2a84e529b79204bc3a78718e335bc
Ben Currie <gameguy000@outlook.com>
2024-11-03 15:20:18 -0800

    css messed with

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var buttonClicked = true; 
-
+console.log("test")
+var i = 1;
 function changeText() {
     const textElement = document.getElementById("text");
-    if (buttonClicked == true){ 
+    if (buttonClicked){ 
+        console.log("button clicked x " + i++)
         textElement.innerText = "You clicked the button!";
         buttonClicked = false;
     }else{
+        console.log("button clicked x "+ i++)
         textElement.innerText = "You clicked it again!"
         buttonClicked = true;
     }
